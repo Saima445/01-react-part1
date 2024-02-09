6b0e1922c7872496f97968dda89c3d97f154426c
SaimaS <saima.salonen@gmail.com>
2024-02-09 14:49:27 +0200

    06 Hello komponenttiin lisätty syntymövuotta laskeva funktio funktion sisälle.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 const Hello = (props) => {
-  console.log(`props name on ${props.name}`);
+  console.log(`props name on ${props.age}`);
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear();
+    return yearNow - props.age;
+  };
+  console.log(bornYear());
   return (
     <section>
       <p>
         Hello {props.name}, you are {props.age} years old
       </p>
+      <p>So you were probably born {bornYear()}</p>
     </section>
   );
 };
