ea141b62ace6e5c2ac1f5ba3dcd4432a2b452869
SaimaS <saima.salonen@gmail.com>
2024-02-11 15:05:03 +0200

    10 runko muutettu laskuriksi tehtävänannon mukaisesti. Main.jsx myös muokattu ja komponentti renderöityy 3 kertaa.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,7 @@
-const Hello = ({ name, age }) => {
-  console.log(`props name and age is ${name} ${age}`);
-  const bornYear = () => new Date().getFullYear() - age;
-  console.log(bornYear());
-  return (
-    <section>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born {bornYear()}</p>
-    </section>
-  );
-};
+const App = (props) => {
+  const { counter } = props;
 
-const App = () => {
-  const nimi = "Pekka";
-  const ika = 10;
-
-  return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={15 + 15} />
-      <Hello name={nimi} age={ika} />
-    </>
-  );
+  return <>{counter}</>;
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,19 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+let counter = 1;
+console.log(counter);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const refresh = () => {
+  root.render(<App counter={counter} />);
+};
+
+refresh();
+counter += 1;
+console.log(counter);
+refresh();
+counter += 1;
+console.log(counter);
+refresh();
