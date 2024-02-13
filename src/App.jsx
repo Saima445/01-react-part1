3d36300422ff082c77dbaf5e060a977807fda99c
SaimaS <saima.salonen@gmail.com>
2024-02-13 12:28:21 +0200

    13 muokattu app-komponentti sisältämään onClick funktion sekä uudet tapahtumankäsittelijä funktiot.

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import { useState } from "react";
 
 const App = () => {
   const [counter, setCounter] = useState(0);
-
-  setTimeout(() => setCounter(counter + 1), 1000);
-
-  console.log("rendering...", counter);
-  return <>{counter}</>;
+  // const handleClick = () => {
+  //   console.log("clicked");
+  // };
+  // setTimeout(() => setCounter(counter + 1), 1000);
+  // console.log("rendering...", counter);
+  return (
+    <>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>plus</button>
+      <button onClick={() => setCounter(0)}>zero</button>
+    </>
+  );
 };
 
 export default App;
