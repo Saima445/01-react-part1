02f7943c60e72b0f3b7ff77c19e5eb15cff020fa
SaimaS <saima.salonen@gmail.com>
2024-02-13 11:57:29 +0200

    12 laskuri suoritetaan nyt useState-funktiolla app-komponenttissa, tämä on parempi tapa laskurin tekemiseen.

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,5 @@
 import ReactDOM from "react-dom/client";
+
 import App from "./App.jsx";
 
-let counter = 1;
-console.log(counter);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const refresh = () => {
-  root.render(<App counter={counter} />);
-};
-
-setInterval(() => {
-  refresh();
-  counter += 1;
-  console.log(counter);
-}, 1000);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
